7aa82cc87b4e138dd5c3fd5f49addb58c737dfd4
cesc <cscherhoff@web.de>
2022-01-31 21:35:38 +0100

    Initial commit

---
+++ src/main/java/com/exxeta/investmentservicems/InvestmentServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.exxeta.investmentservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentServiceMsApplication.class, args);
+	}
+
+}
