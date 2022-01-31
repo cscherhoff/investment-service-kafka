19ed8ae0713ff8562c8b7ef22b88f475ffde240f
cesc <cscherhoff@web.de>
2022-01-31 22:05:47 +0100

    Implement TransactionController

---
+++ src/main/java/com/exxeta/investmentservicems/controller/TransactionController.java
@@ -0,0 +1,53 @@
+package com.exxeta.investmentservicems.controller;
+
+import com.exxeta.investmentservice.entities.Transaction;
+import com.exxeta.investmentservice.service.TransactionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/investments/user/{userId}")
+public class TransactionController {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private final TransactionHandler transactionHandler;
+//    private final AccountService accountService;
+
+    private final Logger logger = LoggerFactory.getLogger(TransactionController.class);
+
+    public TransactionController(TransactionHandler transactionHandler) {
+        this.transactionHandler = transactionHandler;
+//        this.accountService = accountService;
+    }
+
+    @PostMapping(path = "/transaction")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String addNewTransaction(@PathVariable long userId, @Valid @RequestBody Transaction transaction) {
+        logger.info("Received transaction: " + transaction);
+        try {
+            transaction.setUserId(userId);
+            transactionHandler.handleTransaction(transaction);
+//            changeAccountBalance(transaction);
+            return mapper.writeValueAsString(transaction);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+
+//    private void changeAccountBalance(Transaction transaction) {
+//        if (transaction.getType().equals("Buy")) {
+//            accountService.sendAmountToAccountService(transaction.getUserId(), transaction.getDepotName(),
+//                    transaction.getTotalPrice().negate().setScale(2, RoundingMode.HALF_UP));
+//        } else {
+//            accountService.sendAmountToAccountService(transaction.getUserId(), transaction.getDepotName(),
+//                    transaction.getTotalPrice());
+//        }
+//    }
+}
