bdd858ed75130085f6a49e31c808575e57ec3146
cesc <cscherhoff@web.de>
2022-01-31 22:08:03 +0100

    Implement DepotEntryController

---
+++ src/main/java/com/exxeta/investmentservicems/controller/DepotEntryController.java
@@ -0,0 +1,40 @@
+package com.exxeta.investmentservicems.controller;
+
+import com.exxeta.investmentservice.service.InvestmentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping(path = "/investments/user/{userId}")
+public class DepotEntryController {
+
+    private final InvestmentService investmentService;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public DepotEntryController(InvestmentService investmentService) {
+        this.investmentService = investmentService;
+    }
+
+    @GetMapping(path = "/depotEntries")
+    public String getAllDepotEntries(@PathVariable long userId) {
+        try {
+            return mapper.writeValueAsString(investmentService.getAllDepotEntries(userId));
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+
+    @GetMapping(path = "/depotEntries/{depotName}")
+    public String getAllDepotEntriesForAGivenDepot(@PathVariable long userId, @PathVariable String depotName) {
+        try {
+            return mapper.writeValueAsString(investmentService.getAllDepotEntries(userId, depotName));
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        }
+    }
+}
