9ca545c8540e3f57135a8463acb220acffd03627
cesc <cscherhoff@web.de>
2022-04-07 22:06:43 +0200

    Use eureka and feign

--- src/main/java/com/exxeta/investmentservicems/InvestmentServiceMsApplication.java
+++ src/main/java/com/exxeta/investmentservicems/InvestmentServiceMsApplication.java
@@ -2,8 +2,10 @@ package com.exxeta.investmentservicems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.exxeta")
+@EnableFeignClients
 public class InvestmentServiceMsApplication {
 
 	public static void main(String[] args) {
