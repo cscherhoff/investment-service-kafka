b0f4ed4fdb35ed1699454bd28c92717319f4aaf1
cscherho <cscherhoff@web.de>
2022-07-25 17:29:20 +0200

    Add CORS configuration to allow the frontend to call the backend

--- src/main/java/com/exxeta/investmentservicems/configuration/SpringConfiguration.java
+++ src/main/java/com/exxeta/investmentservicems/configuration/SpringConfiguration.java
@@ -3,10 +3,17 @@ package com.exxeta.investmentservicems.configuration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EntityScan(basePackages = {"com.exxeta.investmentservice"})
 @EnableJpaRepositories(basePackages = {"com.exxeta.investmentservice"})
-public class SpringConfiguration {
+public class SpringConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200");
+    }
 
 }
